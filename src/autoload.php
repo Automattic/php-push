f563519c92b0aaf97f234edea2cbbd7391a3238d
Jeremy Massel <jeremy.massel@automattic.com>
2020-07-02 11:02:07 -0600

    Don’t use relative include paths

--- src/autoload.php
+++ src/autoload.php
@@ -3,21 +3,21 @@ declare( strict_types = 1 );
 
 // For use in non-composer environments
 
-require_once 'APNSClient.php';
+require_once __DIR__ . '/APNSClient.php';
 
-require_once 'model/APNSAlert.php';
-require_once 'model/APNSConfiguration.php';
-require_once 'model/APNSCredentials.php';
-require_once 'model/APNSPayload.php';
-require_once 'model/APNSPriority.php';
-require_once 'model/APNSPushType.php';
-require_once 'model/APNSRequest.php';
-require_once 'model/APNSRequestMetadata.php';
-require_once 'model/APNSResponse.php';
-require_once 'model/APNSResponseMetrics.php';
-require_once 'model/APNSSound.php';
+require_once __DIR__ . '/model/APNSAlert.php';
+require_once __DIR__ . '/model/APNSConfiguration.php';
+require_once __DIR__ . '/model/APNSCredentials.php';
+require_once __DIR__ . '/model/APNSPayload.php';
+require_once __DIR__ . '/model/APNSPriority.php';
+require_once __DIR__ . '/model/APNSPushType.php';
+require_once __DIR__ . '/model/APNSRequest.php';
+require_once __DIR__ . '/model/APNSRequestMetadata.php';
+require_once __DIR__ . '/model/APNSResponse.php';
+require_once __DIR__ . '/model/APNSResponseMetrics.php';
+require_once __DIR__ . '/model/APNSSound.php';
 
-require_once 'helpers/HTTPMessageParser.php';
+require_once __DIR__ . '/helpers/HTTPMessageParser.php';
 
-require_once 'signing/APNSTokenFactory.php';
-require_once 'signing/APNSDefaultTokenFactory.php';
+require_once __DIR__ . '/signing/APNSTokenFactory.php';
+require_once __DIR__ . '/signing/APNSDefaultTokenFactory.php';
