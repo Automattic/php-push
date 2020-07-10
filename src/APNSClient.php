3678644ee728fdce285ec10867f94b66b3a96eaf
Gio Lodi <giovanni.lodi42@gmail.com>
2020-07-10 12:15:56 +1000

    Define abstraction in front of cURL
    The intent of this is to decouple `APNSClient` from the implementation
    details of the transport layer it uses, namely cURL.
    
    The immediate benefit of this approach is that we'll then be able to use
    a test double an verify how `APNSClient` behaves for different network
    responses.

--- src/APNSClient.php
+++ src/APNSClient.php
@@ -3,8 +3,8 @@ declare( strict_types = 1 );
 
 class APNSClient {
 
-	/** @var resource */
-	private $curl_handle;
+	/** @var ConnectionManager **/
+	private $connection_manager;
 
 	/** @var APNSConfiguration */
 	private $configuration;
@@ -23,12 +23,8 @@ class APNSClient {
 
 	public function __construct( APNSConfiguration $configuration ) {
 		$this->configuration = $configuration;
-
-		$ch = curl_multi_init();
-		curl_multi_setopt( $ch, CURLMOPT_PIPELINING, CURLPIPE_MULTIPLEX );
-		curl_multi_setopt( $ch, CURLMOPT_MAX_TOTAL_CONNECTIONS, 1 );
-		curl_multi_setopt( $ch, CURLMOPT_MAX_PIPELINE_LENGTH, 1000 );
-		$this->curl_handle = $ch;
+		// TODO: how to inject this?
+		$this->connection_manager = new CURLConnectionManager();
 
 		$this->refreshToken();
 	}
@@ -57,7 +53,7 @@ class APNSClient {
 	}
 
 	public function close(): void {
-		curl_multi_close( $this->curl_handle );
+		$this->connection_manager->closeConnection();
 	}
 
 	public function setDebug( bool $debug ): self {
@@ -73,19 +69,9 @@ class APNSClient {
 	private function enqueueRequest( APNSRequest $request ): void {
 		$headers = $request->getHeadersForConfiguration( $this->configuration );
 		$headers = $this->convertRequestHeaders( $headers );
+		$url = $request->getUrlForConfiguration( $this->configuration );
 
-		$ch = curl_init( $request->getUrlForConfiguration( $this->configuration ) );
-		curl_setopt( $ch, CURLOPT_HEADER, true );
-		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-		curl_setopt( $ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS );
-		curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
-		curl_setopt( $ch, CURLOPT_POST, true );
-		curl_setopt( $ch, CURLOPT_POSTFIELDS, $request->getBody() );
-		curl_setopt( $ch, CURLOPT_VERBOSE, $this->debug );
-		curl_setopt( $ch, CURLOPT_PORT, $this->port_number );
-		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, ! $this->disable_ssl_verification );
-
-		curl_multi_add_handle( $this->curl_handle, $ch );
+		$this->connection_manager->enqueueRequest( $url, $this->port_number, $headers, $request->getBody(), $this->debug, ! $this->disable_ssl_verification );
 	}
 
 	/**
@@ -95,56 +81,25 @@ class APNSClient {
 	 */
 	private function sendQueuedRequests(): array {
 
-		$responses = [];
+		$network_responses = $this->connection_manager->sendQueuedRequests();
 
-		do {
-			$status = curl_multi_exec( $this->curl_handle, $running_operation_count );
+		$apns_responses = [];
 
-			while ( true ) {
-
-				$info = curl_multi_info_read( $this->curl_handle );
-
-				if ( ! $info ) {
-					break;
-				}
-
-				if ( $info['result'] !== CURLE_OK ) {
-					throw new Exception( 'Request failed: ' . $info['result'] );
-				}
-
-				if ( ! is_null( $info['handle'] ) ) {
-					$handle = $info['handle'];
-					$responses[] = $this->processResponse( $handle );
-
-					curl_multi_remove_handle( $this->curl_handle, $handle );
-					curl_close( $handle );
-				}
-			}
-		} while ( $running_operation_count > 0 && $status === CURLM_OK );
-
-		if ( $status !== CURLM_OK ) {
-			throw new Exception( 'Unable to continue sending – ' . curl_multi_strerror( $status ) );
+		foreach ( $network_responses as $response ) {
+			$apns_responses[] = $this->processResponse( $response );
 		}
 
-		return $responses;
+		return $apns_responses;
 	}
 
 	/**
-	 * Parse a completed CURL handle into an APNSResponse object
+	 * Parse a Response from the network layer into an APNSResponse object
 	 *
-	 * @param resource $handle
+	 * @param Response $response
 	 */
-	private function processResponse( $handle ): APNSResponse {
-		// Error Code and Details
-		$status_code = intval( curl_getinfo( $handle, CURLINFO_HTTP_CODE ) );
-		$response_text = curl_multi_getcontent( $handle );
-
-		// Interesting Request Metrics for stats
-		$transfer_time = curl_getinfo( $handle, CURLINFO_TOTAL_TIME_T );
-		$total_bytes = curl_getinfo( $handle, CURLINFO_SIZE_UPLOAD_T );
-
-		$metrics = new APNSResponseMetrics( $total_bytes, $transfer_time );
-		return new APNSResponse( $status_code, $response_text, $metrics );
+	private function processResponse( $response ): APNSResponse {
+		$metrics = new APNSResponseMetrics( $response->total_bytes, $response->transfer_time );
+		return new APNSResponse( $response->status_code, $response->text, $metrics );
 	}
 
 	/**
