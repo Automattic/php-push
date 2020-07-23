4cff50f9c317ee270de06a1e393e72bb7236a08d
Gio Lodi <giovanni.lodi42@gmail.com>
2020-07-23 14:35:22 +1000

    Refine APNSClientTest example into something useful
    Now testing that responses received from the network are properly
    parsed.

--- tests/unit/APNSClientTest.php
+++ tests/unit/APNSClientTest.php
@@ -3,12 +3,28 @@ declare( strict_types = 1 );
 
 class APNSClientTest extends APNSTest {
 
-	public function testThatSendingRequestEnqueuesIt() {
-		$network_service_mock = Mockery::spy( 'MultiplexedNetworkService' );
+	public function testThatRequestsFromNetworkAreProcessed() {
+		$network_service_mock = Mockery::mock( 'MultiplexedNetworkService' );
+		$network_service_mock
+			->shouldReceive( 'enqueueRequest' )
+			->once();
+		$fake_responses = [
+			new Response( 200, $this->new_apns_http_failure_response( 200 ), 1, 1 ),
+			new Response( 400, $this->new_apns_http_failure_response( 400 ), 1, 1 ),
+		];
+		$network_service_mock
+			->shouldReceive( 'sendQueuedRequests' )
+			->andReturn( $fake_responses );
 		$client = new APNSClient( $this->new_configuration(), $network_service_mock );
 
-		$client->sendRequests( [ $this->new_request() ] );
+		$responses = $client->sendRequests( [ $this->new_request() ] );
 
-		$network_service_mock->shouldHaveReceived( 'enqueueRequest' )->once();
+		$this->assertEquals( 2, count( $responses ) );
+		// For the purpose of this test it's enough to verify that the received
+		// requests are mapped to the expected type by means of calling a
+		// method on them. It's then up to the test for the type initialization
+		// to verify the mapping is done correctly.
+		$this->assertEquals( 200, $responses[0]->getStatusCode() );
+		$this->assertEquals( 400, $responses[1]->getStatusCode() );
 	}
 }
