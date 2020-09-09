8a5af2094f04b83ad41e74e8df4d40ff9ef6991b
Jeremy Massel <jeremy.massel@automattic.com>
2020-09-08 23:27:06 -0600

    Fix mock server formatting

--- tests/MockAPNSServer/server.js
+++ tests/MockAPNSServer/server.js
@@ -1,54 +1,70 @@
-const http2 = require("http2");
-const fs = require("fs");
-const uuid = require("uuid");
+const http2 = require( "http2" );
+const fs    = require( "fs" );
+const uuid  = require( "uuid" );
 
-const server = http2.createSecureServer({
-  key: fs.readFileSync("test-key.pem"),
-  cert: fs.readFileSync("test-cert.pem"),
-});
-server.on("error", (err) => console.error(err));
+const server = http2.createSecureServer(
+	{
+		key: fs.readFileSync( "test-key.pem" ),
+		cert: fs.readFileSync( "test-cert.pem" ),
+	}
+);
+
+server.on(
+	"error",
+	function(err){
+		console.error( err )
+	}
+);
 
 var requestNumber = 0;
-var sessions = [];
+var sessions      = [];
 
-server.on("session", (session) => {
-  sessions.push(session);
-});
+server.on(
+	"session",
+	function (session) {
+		sessions.push( session );
+	}
+);
 
-server.on("stream", (stream, headers) => {
+server.on(
+	"stream",
+	function(stream, headers) {
+		stream.respond(
+			{
+				"content-type": "text/html; charset=utf-8",
+				":status": 200,
+				"apns-id": uuid.v4(),
+			}
+		);
+		if (headers[":path"] === "/") {
+			console.log( "Request:", headers );
+			requestNumber++;
+			stream.end( "Request Count: " + requestNumber );
+		}
+		if (headers[":path"] === "/reset") {
+			console.log( "Resetting Sessions. Current Session Count: " + sessions.length );
 
-  stream.respond({
-    "content-type": "text/html; charset=utf-8",
-    ":status": 200,
-    "apns-id": uuid.v4(),
-  });
+			const sessionsCopy = sessions.filter(
+				function(session) {
+					return session !== stream.session
+				}
+			); // Reset all the other sessions except the one for this request
 
-  if (headers[":path"] === "/") {
-    console.log("Request:", headers);
-    requestNumber++;
-    stream.end("Request Count: " + requestNumber);
-  }
+			sessionsCopy.forEach(
+				function (session) {
+					const reason   = Buffer.from( '{"reason": "Foo"}', "utf8" );
+					const streamId = session.state.lastProcStreamID;
+					if ( ! session.closed) {
+						session.goaway( http2.constants.NGHTTP2_NO_ERROR, streamId, reason );
+					}
+				}
+			);
 
-  if (headers[":path"] === "/reset") {
-    console.log("Resetting Sessions. Current Session Count: " + sessions.length);
+			this.sessions = [stream.session];
+			stream.end( `Reset Complete: Current Sessions Count: ${this.sessions.length}` );
+		}
+	}
+);
 
-    const sessionsCopy = sessions.filter(
-      (session) => session !== stream.session
-    ); // Reset all the other sessions except the one for this request
-
-    sessionsCopy.forEach((session) => {
-      const reason = Buffer.from('{"reason": "Foo"}', "utf8");
-      const streamId = session.state.lastProcStreamID;
-
-      if (!session.closed) {
-        session.goaway(http2.constants.NGHTTP2_NO_ERROR, streamId, reason);
-      }
-    });
-
-    sessions = [stream.session];
-    stream.end(`Reset Complete: Current Sessions Count: ${sessions.length}`);
-  }
-});
-
-server.listen(8443);
+server.listen( 8443 );
 console.log(`Server running at https://127.0.0.1:8443`);
